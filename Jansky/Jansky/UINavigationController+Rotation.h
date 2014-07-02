106730ccd9b97a3bdc4e910fc347ff7b7d7a3ef6
Cory Alder <coryalder@gmail.com>
2014-07-02 16:37:01 -0700

    Updated app icon

---
+++ Jansky/Jansky/UINavigationController+Rotation.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+Rotation.h
+//  Jansky
+//
+//  Created by Cory Alder on 2014-07-02.
+//  Copyright (c) 2014 Davander Mobile Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (Rotation)
+
+@end
---
+++ Jansky/Jansky/UINavigationController+Rotation.m
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+Rotation.m
+//  Jansky
+//
+//  Created by Cory Alder on 2014-07-02.
+//  Copyright (c) 2014 Davander Mobile Corporation. All rights reserved.
+//
+
+#import "UINavigationController+Rotation.h"
+
+@implementation UINavigationController (Rotation)
+
+@end
